7d4b5def89e9290f81a7c75bf409b50352dd5806
EdwardLeeMacau <b05901119@ntu.edu.tw>
2022-04-19 21:36:10 +0800

    Implement naive GAN model
    Squashed commit of the following:
    
    commit e2142ab42a548c5484344b813d81f2dfa6c48192
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Wed Apr 13 19:12:48 2022 +0800
    
        WIP: Revise coding style of GAN
    
    commit bef6bf11ee71fc4a2bf2a07c9d856efb2c2326fd
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Mon Apr 11 16:58:06 2022 +0800
    
        WIP: Implement GAN

---
+++ MLSpring2022/hw06_gan/download_dataset.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://huggingface.co/datasets/LeoFeng/MLHW_6
+unzip ./MLHW_6/faces.zip -d .
---
+++ MLSpring2022/hw06_gan/submit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd output
+tar -zcf ../submission.tgz *.jpg
+cd ..
